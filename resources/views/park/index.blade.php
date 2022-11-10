88f25169478d930b89f25e99cc862e79c0e57cc8
Azran Hossain <mr69751@gmail.com>
2022-11-10 12:59:20 +0600

    ui changed - Duration input added

--- resources/views/park/index.blade.php
+++ resources/views/park/index.blade.php
@@ -10,49 +10,30 @@
             <div class="card-body">
                 <h5 class="card-title">1st Floor Park</h5>
                 <ul class="list-unstyled">
-                    <li><i class="fa-solid fa-money-check-dollar"></i> 40 Taka/Hour</li>
-                    <li><i class="fa-solid fa-location-dot"></i> F1</li>
+                    <li><i class="fa-solid fa-money-check-dollar"></i> 5$ /Hour</li>
+                    <li><i class="fa-solid fa-location-dot"></i> 1A</li>
                 </ul>
-                <a href="#" class="p-2 rounded text-decoration-none btn-info">Reserve</a>
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="#" class="p-2 rounded text-decoration-none btn-info">Reserve</a>
+                    <input type="number" name="duration" id="duration" class="form-control w-50" placeholder="Duration">
+                </div>
             </div>
         </div>
         <div class="card m-5" style="width: 18rem;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Subterranean_parking_lot.jpg/1280px-Subterranean_parking_lot.jpg"
+            <img src="https://www.middleeast.weber/files/sodamco/styles/1920x1080_resize/public/2018-05/Parking_floor_solutions_under_commercial_landmark3.jpg"
                 class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">1st Floor Park</h5>
+                <h5 class="card-title">Ground Floor Park</h5>
                 <ul class="list-unstyled">
-                    <li><i class="fa-solid fa-money-check-dollar"></i> 40 Taka/Hour</li>
-                    <li><i class="fa-solid fa-location-dot"></i> F1</li>
+                    <li><i class="fa-solid fa-money-check-dollar"></i> 5$ /Hour</li>
+                    <li><i class="fa-solid fa-location-dot"></i> 1B</li>
                 </ul>
-                <a href="#" class="p-2 rounded text-decoration-none btn-info">Reserve</a>
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="#" class="p-2 rounded text-decoration-none btn-info">Reserve</a>
+                    <input type="number" name="duration" id="duration" class="form-control w-50" placeholder="Duration">
+                </div>
             </div>
         </div>
-        <div class="card m-5" style="width: 18rem;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Subterranean_parking_lot.jpg/1280px-Subterranean_parking_lot.jpg"
-                class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">1st Floor Park</h5>
-                <ul class="list-unstyled">
-                    <li><i class="fa-solid fa-money-check-dollar"></i> 40 Taka/Hour</li>
-                    <li><i class="fa-solid fa-location-dot"></i> F1</li>
-                </ul>
-                <a href="#" class="p-2 rounded text-decoration-none btn-info">Reserve</a>
-            </div>
-        </div>
-        <div class="card m-5" style="width: 18rem;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Subterranean_parking_lot.jpg/1280px-Subterranean_parking_lot.jpg"
-                class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">1st Floor Park</h5>
-                <ul class="list-unstyled">
-                    <li><i class="fa-solid fa-money-check-dollar"></i> 40 Taka/Hour</li>
-                    <li><i class="fa-solid fa-location-dot"></i> F1</li>
-                </ul>
-                <a href="#" class="p-2 rounded text-decoration-none btn-info">Reserve</a>
-            </div>
-        </div>
-
 
     </div>
 @endsection
