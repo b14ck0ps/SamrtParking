e7380ef8720a4687378c22bdda55004b9bd4a6cd
Azran Hossain <mr69751@gmail.com>
2022-12-08 12:50:32 +0600

    reg api

---
+++ app/Http/Controllers/Api/RegistrationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegistrationController extends Controller
+{
+    public function registration(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'vehicle_name' => 'required',
+            'type' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'vehicle_name' => $request->vehicle_name,
+            'type' => $request->type,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully created user!',
+            'user' => $user,
+        ], 201);
+    }
+}
