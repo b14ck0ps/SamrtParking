2d0ea46c58fd224dd3d0f71e08ad3be6217c0790
Azran Hossain <mr69751@gmail.com>
2022-12-08 13:27:07 +0600

    bokking by api

---
+++ app/Http/Controllers/Api/BookingController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Bookings;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BookingController extends Controller
+{
+    public function book(Request $request, $id)
+    {
+        $request->validate([
+            'parking_slot' => 'required',
+            'vehicle_number' => 'required',
+            'duration' => 'required',
+        ]);
+        $booking = new Bookings([
+            'parking_slot' => $request->get('parking_slot'),
+            'vehicle_number' => $request->get('vehicle_number'),
+            'duration' => $request->get('duration'),
+            'user_id' => $id,
+        ]);
+        $booking->save();
+        return response()->json('Booking Successful');
+    }
+}
