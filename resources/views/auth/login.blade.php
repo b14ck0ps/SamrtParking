fd315447903f21b7a3bf9ae5158ac5245de32159
Azran Hossain <mr69751@gmail.com>
2022-12-10 15:51:10 +0600

    rm blade temps

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,34 +2,4 @@
 @section('content')
     @csrf
     <div id="login"></div>
-    {{-- <div class="container h-100 mt-5">
-        <div class="row h-100 justify-content-center align-items-center  card p-5">
-            <form class="col-6" method="POST" action="login">
-                @csrf
-                @if (session('status'))
-                    <div class="alert alert-danger">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                <div class="form-group">
-                    <label for="inputEmail">Email</label>
-                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" name="email">
-                    @error('email')
-                        <div class="alert alert-danger mt-2">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword">Password</label>
-                    <input type="password" class="form-control" id="inputPassword" placeholder="Password" name="password">
-                    @error('password')
-                        <div class="alert alert-danger mt-2">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label class="form-check-label"><input type="checkbox"> Remember me</label>
-                </div>
-                <button type="submit" class="btn-primary">Sign in</button>
-            </form>
-        </div>
-    </div> --}}
 @endsection
